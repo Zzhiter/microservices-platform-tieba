b95dae890a11f71c4d4aecca05051ecf67d6f605
Zzhiter <Zz_hiter@163.com>
2023-12-02 13:02:40 +0800

    first commit

---
+++ buaa-web/layui-web/src/main/java/com/central/web/BackWebApplication.java
@@ -0,0 +1,14 @@
+package com.central.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author buaa
+ */
+@SpringBootApplication
+public class BackWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackWebApplication.class, args);
+	}
+}
---
+++ buaa-web/react-web/src/main/frontend/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import { useIntl } from 'umi';
+
+const Footer: React.FC = () => {
+  const intl = useIntl();
+  const defaultMessage = intl.formatMessage({
+    id: 'app.copyright.produced',
+    // defaultMessage: 'zlt版权所有：https://gitee.com/zlt2000/microservices-platform',
+    defaultMessage: 'buaa-soft',
+  });
+
+  // const currentYear = new Date().getFullYear();
+
+  return (
+    <DefaultFooter
+      copyright={`${2023} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/zlt2000/microservices-platform',
+          blankTarget: true,
+        },
+        {
+          key: 'beian',
+          title: '粤ICP备19081412号',
+          href: 'https://beian.miit.gov.cn/',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
